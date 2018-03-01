2c4909fbe1fc9d893534a0754f6bca9c2c40a48a
Hengruo Zhang <hengruoz@gmail.com>
2018-02-28 23:45:47 -0500

    new file:   model.py

---
+++ model.py
@@ -0,0 +1,11 @@
+import torch
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+class RNet(nn.Module):
+    pass
+
+class Encoder(nn.Module):
+    def __init__()
