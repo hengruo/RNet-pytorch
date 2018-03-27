7aa09ed7a70c5e5f4614c8fd892a3ed86195b22d
hengruo <hengruoz@gmail.com>
2018-03-26 22:38:03 -0400

    modified:   main.py

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import argparse
+from data import get_dataset
+from data import SQuAD
+from model import RNet
+import torch
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import config as cfg
 
 def parse_args():
     args = argparse.ArgumentParser(description="An R-net implementation.")
@@ -8,12 +17,24 @@ def parse_args():
     args.add_argument("--num_steps", dest='num_steps', type=int, default="60000")
     return args.parse_args()
 
+def to_tensor(pack, data):
+    # tensor representation of passage, question, answer 
+    p, q, a = None, None, None
+    return p, q, a
+
+def train(data: SQuAD):
+    model = RNet().cuda()
+    for pack in data.train.pack:
+        p, q, a = to_tensor(pack, data)
+        output = model(p, q)
+        loss = F.cross_entropy(output, a)
+        optimizer = optim.Adadelta(loss)
+
 def main():
     args = parse_args()
-    if args.mode == 'all':
-        pass
-    else:
-        pass
+    data = get_dataset()
+    train(data)
+
 
 if __name__ == '__main__':
     main()
